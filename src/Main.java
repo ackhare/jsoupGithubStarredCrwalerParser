9401a9c51cf3bc3790c0b1d737cf66ef20ecded7
ac38591 <chetan.khare@centurylink.com>
2019-10-24 16:38:04 +0530

    extracted stars description etc

--- src/Main.java
+++ src/Main.java
@@ -13,16 +13,38 @@ public class Main {
 		while(true)
 		{
 		 doc = Jsoup.connect(url).get();
-		Elements resultsH3 = doc.select("h3 > a");
-		Elements nnn = doc.select("a.BtnGroup-item");
-		 System.out.println(nnn.attr("href"));
-		for (Element link : resultsH3) {
+		Elements giturl = doc.select("h3 > a");
+		Elements nextLinks = doc.select("a.BtnGroup-item");
+		
+		Elements description=doc.select("div.d-block > div.py-1");
+		Elements primaryTag=doc.select("div.d-block > div.text-gray > span.mr-3");
+		Elements starsCurrent=doc.select("div.d-block > div.text-gray > span.mr-3").next("a.muted-link");
+		String lastUpdated=doc.select("div.d-block > div.text-gray").attr("relative-time");
+		for (Element link : giturl) {
 		  String linkHref = link.attr("href");
 		  String linkText = link.text();
+		  
 		  System.out.println("Text::"+linkText+", URL::"+linkHref);
 		}
+		 System.out.println("mmmmmmmmmmmmmmmmm");
+		for (Element link : description) {
+			  String linkHref = link.attr("href");
+			  String linkText = link.text();
+			  
+			  System.out.println("Text::"+linkText);
+			}
+		
+		 System.out.println("lololololololololo");
+		for (Element link : primaryTag) {
+			  String linkHref = link.attr("href");
+			  String linkText = link.text();
+			  
+			  System.out.println("Text::"+linkText);
+			}
+		
 		url=null;
-		for (Element link : nnn) {
+		for (Element link : nextLinks) {
+			 
 			  String linkHref = link.attr("href");
 			  String linkText = link.text();
 			  if(linkText.equals("Next"))
