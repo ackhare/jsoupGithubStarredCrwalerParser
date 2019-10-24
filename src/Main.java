a52fe85cd925c9f7b2aad8b9bae19450a7e31084
ac38591 <chetan.khare@centurylink.com>
2019-10-24 14:13:08 +0530

    page number is dynamic now

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,11 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 public class Main {
 	public static void main(String[] args) throws IOException {
+		int numberOfPages=0;
 		String url="https://github.com/ackhare?tab=stars";
 		 Document doc = Jsoup.connect(url).get();
-//		 for (Element link : resultsH3) {
-//			  String linkHref = link.attr("href");
-//			  String linkText = link.text();
-//			  System.out.println("Text::"+linkText+", URL::"+linkHref);
-//			}
-		for(int i=0;i<=10;i++)
+
+		while(true)
 		{
 		 doc = Jsoup.connect(url).get();
 		Elements resultsH3 = doc.select("h3 > a");
@@ -24,12 +21,17 @@ public class Main {
 		  String linkText = link.text();
 		  System.out.println("Text::"+linkText+", URL::"+linkHref);
 		}
+		url=null;
 		for (Element link : nnn) {
 			  String linkHref = link.attr("href");
 			  String linkText = link.text();
 			  if(linkText.equals("Next"))
+			  {
 				url=linkHref;
 			}
+		}
+		if(url==null)
+			break;
 		
 		}
 	}
